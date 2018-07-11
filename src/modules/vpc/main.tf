efea32bc362ce6c11ef38c0627e82e803465f53c
ghpua <ghpua@outlook.com>
2018-07-11 23:46:45 +0000

    refactor vpc module

--- src/modules/vpc/main.tf
+++ src/modules/vpc/main.tf
@@ -22,48 +22,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-variable "cidr" {
-  description = "The CIDR block for the VPC."
-}
-
-variable "external_subnets" {
-  description = "List of external subnets"
-  type        = "list"
-}
-
-variable "internal_subnets" {
-  description = "List of internal subnets"
-  type        = "list"
-}
-
-variable "environment" {
-  description = "Environment tag, e.g prod"
-}
-
-variable "availability_zones" {
-  description = "List of availability zones"
-  type        = "list"
-}
-
-variable "name" {
-  description = "Name tag, e.g stack"
-  default     = "stack"
-}
-
-variable "use_nat_instances" {
-  description = "If true, use EC2 NAT instances instead of the AWS NAT gateway service."
-  default     = false
-}
-
-variable "nat_instance_type" {
-  description = "Only if use_nat_instances is true, which EC2 instance type to use for the NAT instances."
-  default     = "t2.nano"
-}
-
-variable "use_eip_with_nat_instances" {
-  description = "Only if use_nat_instances is true, whether to assign Elastic IPs to the NAT instances. IF this is set to false, NAT instances use dynamically assigned IPs."
-  default     = false
-}
 
 # This data source returns the newest Amazon NAT instance AMI
 data "aws_ami" "nat_ami" {
@@ -78,11 +36,6 @@ data "aws_ami" "nat_ami" {
     name   = "name"
     values = ["amzn-ami-vpc-nat*"]
   }
-}
-
-variable "nat_instance_ssh_key_name" {
-  description = "Only if use_nat_instance is true, the optional SSH key-pair to assign to NAT instances."
-  default     = ""
 }
 
 /**
@@ -291,51 +244,3 @@ resource "aws_route_table_association" "external" {
   route_table_id = "${aws_route_table.external.id}"
 }
 
-/**
- * Outputs
- */
-
-// The VPC ID
-output "id" {
-  value = "${aws_vpc.main.id}"
-}
-
-// The VPC CIDR
-output "cidr_block" {
-  value = "${aws_vpc.main.cidr_block}"
-}
-
-// A comma-separated list of subnet IDs.
-output "external_subnets" {
-  value = ["${aws_subnet.external.*.id}"]
-}
-
-// A list of subnet IDs.
-output "internal_subnets" {
-  value = ["${aws_subnet.internal.*.id}"]
-}
-
-// The default VPC security group ID.
-output "security_group" {
-  value = "${aws_vpc.main.default_security_group_id}"
-}
-
-// The list of availability zones of the VPC.
-output "availability_zones" {
-  value = ["${aws_subnet.external.*.availability_zone}"]
-}
-
-// The internal route table ID.
-output "internal_rtb_id" {
-  value = "${join(",", aws_route_table.internal.*.id)}"
-}
-
-// The external route table ID.
-output "external_rtb_id" {
-  value = "${aws_route_table.external.id}"
-}
-
-// The list of EIPs associated with the internal subnets.
-output "internal_nat_ips" {
-  value = ["${aws_eip.nat.*.public_ip}"]
-}
---
+++ src/modules/vpc/variables.tf
@@ -0,0 +1,48 @@
+variable "cidr" {
+  description = "The CIDR block for the VPC."
+}
+
+variable "external_subnets" {
+  description = "List of external subnets"
+  type        = "list"
+}
+
+variable "internal_subnets" {
+  description = "List of internal subnets"
+  type        = "list"
+}
+
+variable "environment" {
+  description = "Environment tag, e.g prod"
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = "list"
+}
+
+variable "name" {
+  description = "Name tag, e.g stack"
+  default     = "stack"
+}
+
+variable "use_nat_instances" {
+  description = "If true, use EC2 NAT instances instead of the AWS NAT gateway service."
+  default     = false
+}
+
+variable "nat_instance_type" {
+  description = "Only if use_nat_instances is true, which EC2 instance type to use for the NAT instances."
+  default     = "t2.nano"
+}
+
+variable "use_eip_with_nat_instances" {
+  description = "Only if use_nat_instances is true, whether to assign Elastic IPs to the NAT instances. IF this is set to false, NAT instances use dynamically assigned IPs."
+  default     = false
+}
+
+variable "nat_instance_ssh_key_name" {
+  description = "Only if use_nat_instance is true, the optional SSH key-pair to assign to NAT instances."
+  default     = ""
+}
+
